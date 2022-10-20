d3ac15d638deeafa267bd98fc666261fd78eff60
nunezMyles <nunez.myles@gmail.com>
2022-10-20 22:31:46 +0800

    bottom nav bar ui

--- lib/widgets/bottom_navBar.dart
+++ lib/widgets/bottom_navBar.dart
@@ -7,58 +7,61 @@ class BottomNavBar extends StatefulWidget {
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
+int _selectedIndex = 0;
+
 class _BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      switch(index) {
-        case 0:
-          Navigator.pushReplacementNamed(context, '/home');
-          break;
-        case 1:
-          Navigator.pushReplacementNamed(context, '/login');
-          break;
-        case 2:
-          Navigator.pushReplacementNamed(context, '/register');
-          break;
-        case 3:
-          Navigator.pushReplacementNamed(context, '/login');
-          break;
-        default:
-          break;
-      }
     });
+
+    switch(index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 3:
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      default:
+        break;
+    }
+
+
   }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      backgroundColor: Colors.blue.shade600,
+      type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
-            backgroundColor: Colors.red,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
             label: 'Business',
-            backgroundColor: Colors.green,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.school),
             label: 'School',
-            backgroundColor: Colors.purple,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
-            backgroundColor: Colors.pink,
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.lightBlueAccent,
         onTap: _onItemTapped,
       );
   }
