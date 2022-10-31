5243acc1c2e99135fd040bdde16f67924145a65f
nunezMyles <nunez.myles@gmail.com>
2022-10-31 16:03:30 +0800

    git ignore

--- lib/models/global_variables.dart
+++
@@ -1,6 +0,0 @@
-
-String webServerUri = 'https://helpful-seer-366001.as.r.appspot.com/';
-
-String spotifyClientID = 'd257f48e512e45e98096ea7b9f48abad';
-
-String spotifyRedirectUrl = 'https://helpful-seer-366001.as.r.appspot.com';
